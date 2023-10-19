3bf8bc70991e1cc6f95aa2ffe36c541ff8bfca0b
gauransh18 <66783709+gauransh18@users.noreply.github.com>
2023-10-20 03:48:51 +0530

    basic app complete💪

--- lib/pages/weatherPage.dart
+++ lib/pages/weatherPage.dart
@@ -1,4 +1,11 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:hello_weather/api_key.dart';
+import 'package:hello_weather/service/weatherService.dart';
+import 'package:lottie/lottie.dart';
+
+import '../models/weatherModel.dart';
 
 class WeatherHome extends StatefulWidget {
   const WeatherHome({super.key});
@@ -8,8 +15,86 @@ class WeatherHome extends StatefulWidget {
 }
 
 class _WeatherHomeState extends State<WeatherHome> {
+  final _weatherService = WeatherService(apiKey);
+  Weather? _weather;
+  String dayOrNight = "";
+
+  _fetchWeather() async {
+    final cityName = await _weatherService.getCurrentCity();
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      final don = await _weatherService.checkDayOrNight();
+      setState(() {
+        _weather = weather;
+        dayOrNight = don;
+      });
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+
+  String _getWeatherAnimation(String? mainCondition, String dayOrNight) {
+    if (mainCondition == null) return "assets/sun.json";
+
+    final isNight = dayOrNight.toLowerCase() == "night";
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+        return isNight ? "assets/cloudy-night.json" : "assets/cloudy-day.json";
+      case 'rain':
+        return isNight ? "assets/rainy-night.json" : "assets/rainy-day.json";
+      case 'smoke':
+        return "assets/cloudy.json";
+      case 'haze':
+        return "assets/cloudy-butnot.json";
+      case 'dust':
+        return "assets/cloudy-butnot.json";
+      case 'fog':
+        return "assets/cloudy-butnot.json";
+      case 'drizzle':
+        return "assets/cloudy-butnot.json";
+      case 'shower rain':
+        return "assets/rainy.json";
+      case 'thunderstorm':
+        return "assets/thunder.json";
+      case 'clear':
+        return isNight ? "assets/clear-night.json" : "assets/sun.json";
+      default:
+        return isNight ? "assets/clear-night.json" : "assets/sun.json";
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: SafeArea(
+      child: Center(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 50),
+              Text(_weather?.cityName ?? "loading city.."),
+              Spacer(),
+              Lottie.asset(
+                '${_getWeatherAnimation(_weather?.mainCondition, dayOrNight)}',
+              ),
+              Spacer(),
+              Text("${_weather?.temperature.round().toString()} °C",
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    
+                  )),
+              Text("${_weather?.mainCondition ?? "loading.."}"),
+              SizedBox(height: 50),
+            ]),
+      ),
+    ));
   }
 }
--- lib/service/weatherService.dart
+++ lib/service/weatherService.dart
@@ -6,6 +6,9 @@ import 'package:geolocator/geolocator.dart';
 import '../models/weatherModel.dart';
 import 'package:http/http.dart' as http;
 
+import 'package:intl/intl.dart';
+
+
 class WeatherService {
   static const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
   final String apiKey;
@@ -41,12 +44,92 @@ class WeatherService {
       desiredAccuracy: LocationAccuracy.high,
     );
 
-    List<Placemark> placemakrs =
+    List<Placemark> placemarks =
         await placemarkFromCoordinates(position.latitude, position.longitude);
 
-    
-    String? city = placemakrs[0].locality;
+    String? city = placemarks[0].locality;
 
     return city ?? "";
   }
+
+  // Future<String> checkDayOrNight() async {
+  //   // Get the user's current location (you can replace this with any location)
+  //   Position position = await Geolocator.getCurrentPosition(
+  //       desiredAccuracy: LocationAccuracy.high);
+
+  //   // Make an API request to the Sunrise-Sunset API
+  //   String apiUrl =
+  //       'https://api.sunrise-sunset.org/json?lat=${position.latitude}&lng=${position.longitude}';
+  //   var response = await http.get(Uri.parse(apiUrl));
+
+  //   if (response.statusCode == 200) {
+  //     Map data = json.decode(response.body);
+  //     String sunriseTime = data['results']['sunrise'];
+  //     String sunsetTime = data['results']['sunset'];
+
+  //     DateTime currentTime = DateTime.now();
+  //     DateTime sunrise = DateTime.parse(sunriseTime);
+  //     DateTime sunset = DateTime.parse(sunsetTime);
+
+  //     if (currentTime.isAfter(sunrise) && currentTime.isBefore(sunset)) {
+  //       return "day";
+  //     } else {
+  //       return "night";
+  //     }
+  //   } else {
+  //     return "day";
+  //   }
+  // }
+
+
+
+// ...
+
+Future<String> checkDayOrNight() async {
+  // Get the user's current location (you can replace this with any location)
+  Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high);
+
+  // Make an API request to the Sunrise-Sunset API
+  String apiUrl =
+      'https://api.sunrise-sunset.org/json?lat=${position.latitude}&lng=${position.longitude}';
+  var response = await http.get(Uri.parse(apiUrl));
+
+  if (response.statusCode == 200) {
+    Map data = json.decode(response.body);
+    String sunriseTime = data['results']['sunrise'];
+    String sunsetTime = data['results']['sunset'];
+
+    // Regular expressions to match different date formats
+    RegExp amPmRegex = RegExp(r'\d+:\d+:\d+ (AM|PM)');
+    RegExp iso8601Regex = RegExp(r'\d{4}-\d{2}-\d{2}T\d+:\d+:\d+\+\d+:\d+');
+
+    DateTime currentTime = DateTime.now();
+    DateTime sunrise;
+    DateTime sunset;
+
+    if (amPmRegex.hasMatch(sunriseTime) && amPmRegex.hasMatch(sunsetTime)) {
+      // Handle AM/PM date format
+      sunrise = DateFormat('h:mm:ss a').parse(sunriseTime);
+      sunset = DateFormat('h:mm:ss a').parse(sunsetTime);
+    } else if (iso8601Regex.hasMatch(sunriseTime) && iso8601Regex.hasMatch(sunsetTime)) {
+      // Handle ISO 8601 date format
+      sunrise = DateTime.parse(sunriseTime);
+      sunset = DateTime.parse(sunsetTime);
+    } else {
+      // Unable to parse the date, assume it's day
+      return "day";
+    }
+
+    if (currentTime.isAfter(sunrise) && currentTime.isBefore(sunset)) {
+      return "day";
+    } else {
+      return "night";
+    }
+  } else {
+    return "day";
+  }
+}
+
+
 }
